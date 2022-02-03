ed8e21f1e7ac8962dc12cbec54803275ea34ea11
Thorben Westerhuys <thorben@cividi.ch>
2022-02-03 11:47:38 +0100

    project list scraping

---
+++ Makefile
@@ -0,0 +1,2 @@
+scrape:
+	cd densipedia && scrapy crawl densipedia-beispiele -O data/densipedia.json -O data/densipedia.csv
---
+++ densipedia/densipedia/spiders/densipedia_beispiele.py
@@ -0,0 +1,29 @@
+import scrapy
+
+
+class DensipediaBeispieleSpider(scrapy.Spider):
+    name = 'densipedia-beispiele'
+    allowed_domains = ['densipedia.ch/gute-beispiele']
+    start_urls = ['https://densipedia.ch/gute-beispiele/']
+
+    def parse(self, response):
+        articles = response.css('div.view-map-results article')
+        for article in articles:
+            location = article.css('.location::text').get().strip()
+            city = ""
+            canton = ""
+            if ", " in location:
+                components = location.split(", ")
+                if len(components) > 0:
+                    city = components[0]
+                if len(components) > 1:
+                    canton = components[-1]
+            yield {
+                'slug': article.css('a::attr(href)').get().replace('/','',1),
+                'title': article.css('a span::text').get(),
+                'location': location,
+                'city': city,
+                'canton': canton,
+                'lat': article.css('.location::attr(data-location-lat)').get(),
+                'lng': article.css('.location::attr(data-location-lng)').get(),
+            }
